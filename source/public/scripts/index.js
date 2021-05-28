fe3f0a53767f7fb804462086b451f9f3068f51dd
Joel Schneider <schneider-joel@gmx.ch>
2021-05-28 19:13:41 +0200

    defined popup and input

--- source/public/scripts/index.js
+++ source/public/scripts/index.js
@@ -1,5 +1,19 @@
-const selector = document.getElementById('color-style');
+const selColorStyle = document.querySelector('#sel-color-style');
+const btnNewNote = document.querySelector('#btn-new-note');
+const btnClosePopUp = document.querySelector('#btn-close-pop-up');
 
-selector.onchange = function togglePageStyle(){
+function toggleColorStyle(){
     document.body.classList.toggle('dark-mode');
-};
+}
+
+function openNewNotePopUp(){
+    document.querySelector('.popup-container').style.display = 'flex';
+}
+
+function closeNewNotePopUp(){
+    document.querySelector('.popup-container').style.display = 'none';
+}
+
+selColorStyle.addEventListener('change', toggleColorStyle);
+btnNewNote.addEventListener('click', openNewNotePopUp);
+btnClosePopUp.addEventListener('click', closeNewNotePopUp);
